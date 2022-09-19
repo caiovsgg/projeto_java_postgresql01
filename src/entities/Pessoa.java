88a7da00729a73eff5474afe6e93df15f762f9d3
caiovsgg <caiovsg@gmail.com>
2022-09-19 19:45:45 -0300

    first commit

---
+++ src/entities/Pessoa.java
@@ -0,0 +1,61 @@
+package entities;
+
+public class Pessoa {
+
+	private Integer idPessoa;
+	private String nome;
+	private String cpf;
+	private String email;
+
+	public Pessoa() {
+
+	}
+
+	public Pessoa(Integer idPessoa, String nome, String cpf, String email) {
+		super();
+		this.idPessoa = idPessoa;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+	}
+
+	public Integer getIdPessoa() {
+
+		return idPessoa;
+	}
+
+	public void setIdPessoa(Integer idPessoa) {
+		this.idPessoa = idPessoa;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	// Sobrescrita de métido
+	@Override
+	public String toString() {
+		return "Pessoa [idPessoa=" + idPessoa + ", nome=" + nome + ", cpf=" + cpf + ", email=" + email + "]";
+	}
+
+}
